ec0ce983aafaa0668bbf4f2add26bcd130df6775
Kirk Bushell <kirk@awardforce.com>
2019-11-22 09:17:26 +1100

    Fixed js bug adn added close to authenticator

--- public/js/app.js
+++ public/js/app.js
@@ -1870,6 +1870,9 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
@@ -1882,6 +1885,9 @@ __webpack_require__.r(__webpack_exports__);
     };
   },
   methods: {
+    close: function close() {
+      this.visible = false;
+    },
     submitEmail: function submitEmail() {
       var _this = this;
 
@@ -20431,9 +20437,44 @@ var render = function() {
             "div",
             {
               staticClass:
-                "p-8 mt-40 bg-black md:rounded-lg md:w-1/2 md:mx-auto"
+                "relative p-8 mt-40 bg-black md:rounded-lg md:w-1/2 md:mx-auto"
             },
             [
+              _c("div", { staticClass: "absolute top-0 right-0" }, [
+                _c(
+                  "a",
+                  {
+                    attrs: { href: "" },
+                    on: {
+                      click: function($event) {
+                        $event.preventDefault()
+                        return _vm.close()
+                      }
+                    }
+                  },
+                  [
+                    _c(
+                      "svg",
+                      {
+                        staticClass: "fill-current h-8 w-8 mt-2 mr-2",
+                        attrs: {
+                          xmlns: "http://www.w3.org/2000/svg",
+                          viewBox: "0 0 24 24"
+                        }
+                      },
+                      [
+                        _c("path", {
+                          attrs: {
+                            d:
+                              "M4.93 19.07A10 10 0 1119.07 4.93 10 10 0 014.93 19.07zm1.41-1.41A8 8 0 1017.66 6.34 8 8 0 006.34 17.66zM13.41 12l1.42 1.41a1 1 0 11-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 11-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 111.42-1.42L12 10.6l1.41-1.42a1 1 0 111.42 1.42L13.4 12z"
+                          }
+                        })
+                      ]
+                    )
+                  ]
+                )
+              ]),
+              _vm._v(" "),
               _c("h1", { staticClass: "font-serif text-xl text-gray-300" }, [
                 _vm._v("Login")
               ]),
@@ -20576,7 +20617,7 @@ var render = function() {
       })
     ]),
     _vm._v(" "),
-    _vm.cards.length
+    _vm.cards
       ? _c("div", [
           _c(
             "div",
