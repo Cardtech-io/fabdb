fd4a5608e445d3d5b9ceb7f5f07a51aa1ff97e92
Kirk Bushell <kirk@awardforce.com>
2019-11-29 10:12:42 +1100

    Updated tracker to track events properly.

--- resources/js/Components/Tracker.js
+++ resources/js/Components/Tracker.js
@@ -1,7 +1,12 @@
 export default {
     track: function(category, action, label, value) {
         if (typeof ga !== 'undefined') {
-            ga('send', 'event', category, action, label, value);
+            ga('send', 'event', {
+                eventCategory: category,
+                eventAction: action,
+                eventLabel: label,
+                eventValue: value
+            });
         }
     }
 };
