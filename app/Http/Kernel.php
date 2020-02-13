526d4b6cb4fdca67d6f0909299d9c32c7dac3514
Kirk Bushell <kirk@awardforce.com>
2020-02-13 17:10:52 +1300

    Prerender logic added for middleware requirements.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            \FabDB\Http\Middleware\Prerender::class,
             \FabDB\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
