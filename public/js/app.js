4ce9a83122464602c52a0285e58cd211c17905b3
Kirk Bushell <kirk@awardforce.com>
2020-02-20 19:57:33 +1100

    Testing without using custom renderer

--- public/js/app.js
+++ public/js/app.js
@@ -70901,6 +70901,27 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
       });
     },
     newParser: function newParser(string) {
+      var renderer = new marked__WEBPACK_IMPORTED_MODULE_0___default.a.Renderer();
+
+      renderer.paragraph = function (text) {
+        return '<p class="my-4">' + text + '</p>';
+      };
+
+      renderer.link = function (href, title, text) {
+        return '<a href="' + href + '" title="' + title + '" class="link">' + text + '</a>';
+      }; // First we're gonna search for custom syntax
+
+
+      var regexp = /#\[cards\]\((([A-Z]{3}[0-9]{3},?)+)\)/g;
+
+      var matches = _toConsumableArray(string.matchAll(regexp))[0][1].split(',');
+
+      for (var i = 0; i < matches.length; i++) {
+        matches[i] = '<img src="' + this.cardUrl(matches[i], 300) + '" class="inline-block sm:mr-8 rounded-lg sm:rounded-xl my-4">';
+      }
+
+      var content = '<div class="text-center">' + matches.join('\n') + '</div>';
+      string = string.replace(regexp, content);
       return marked__WEBPACK_IMPORTED_MODULE_0___default()(string);
     }
   }
--- resources/js/Utilities/Content.js
+++ resources/js/Utilities/Content.js
@@ -29,6 +29,28 @@ export default {
         },
 
         newParser: function(string) {
+            const renderer = new marked.Renderer();
+
+            renderer.paragraph = function(text) {
+                return '<p class="my-4">' + text + '</p>';
+            };
+
+            renderer.link = function(href, title, text) {
+                return '<a href="' + href + '" title="' + title + '" class="link">' + text + '</a>';
+            };
+
+            // First we're gonna search for custom syntax
+            let regexp = /#\[cards\]\((([A-Z]{3}[0-9]{3},?)+)\)/g;
+            let matches = [...string.matchAll(regexp)][0][1].split(',');
+
+            for (let i = 0; i < matches.length; i++) {
+                matches[i] = '<img src="' + this.cardUrl(matches[i], 300) + '" class="inline-block sm:mr-8 rounded-lg sm:rounded-xl my-4">';
+            }
+
+            let content = '<div class="text-center">' + matches.join('\n') + '</div>'
+
+            string = string.replace(regexp, content);
+
             return marked(string);
         }
     }
