62d851476e9ff831da19fac3ef38f78afc88a58d
Kirk Bushell <kirk@awardforce.com>
2020-07-30 13:01:30 +1000

    Added deck card relationships.

--- app/Domain/Decks/DeckCard.php
+++ app/Domain/Decks/DeckCard.php
@@ -1,9 +1,20 @@
 <?php
 namespace FabDB\Domain\Decks;
 
+use FabDB\Domain\Cards\Card;
 use FabDB\Library\Model;
 
 class DeckCard extends Model
 {
     protected $table = 'deck_cards';
+
+    public function card()
+    {
+        return $this->belongsTo(Card::class);
+    }
+
+    public function deck()
+    {
+        return $this->belongsTo(Deck::class);
+    }
 }
