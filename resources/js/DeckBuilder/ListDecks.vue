74769ae850d1dc4090495f50e0d5eaf774a2690d
Kirk Bushell <kirk@awardforce.com>
2020-08-06 20:13:09 +1000

    Upgrade button added to list decks page for non-subscribers.

--- resources/js/DeckBuilder/ListDecks.vue
+++ resources/js/DeckBuilder/ListDecks.vue
@@ -2,10 +2,18 @@
     <div class="container px-4 sm:mx-auto text-white md:flex">
         <div class="py-8 md:my-20 md:w-1/2 sm:pr-8">
             <h1 class="font-serif text-4xl lg:text-4xl uppercase">Deck Builder</h1>
-            <p class="mt-4">The first and only Flesh &amp; Blood Deck Builder is here. You can create, craft, modify or remove your decks
-                for constructed play, and then export them to PDF for tournament registration, or integration with Tabletop Simulator.</p>
+            <p class="mt-4">
+                The first and only Flesh &amp; Blood Deck Builder is here. You can build your decks
+                for constructed or blitz formats, and then export them to PDF for tournament registration, or
+                integration with Tabletop Simulator.
+            </p>
 
-            <p class="mt-4">If you're a <router-link to="/support" class="link-white">supporter</router-link>, you'l get access to our premium deck builder.</p>
+            <p class="mt-45">
+                <router-link to="/support" class="link-white">Supporters</router-link> gain access to the
+                premium deck builder, deck tester and more.
+            </p>
+
+            <router-link to="/support" class="appearance-none block w-full mt-2 bg-orange-700 text-white rounded-lg py-3 px-4 leading-tight focus:outline-none hover:bg-orange-500 text-center mt-4" v-if="user && !user.subscription">Upgrade to premium</router-link>
         </div>
 
         <div class="md:my-20 md:flex-grow md:w-1/2">
@@ -19,7 +27,7 @@
                     <li class="bg-semi-black rounded-lg mb-2 hover:bg-black" v-for="(deck, key) in decks">
                         <div class="flex">
                             <router-link :to="'/decks/build/premium/' + deck.slug" class="block link-white flex-1 p-4" v-if="user.subscription">{{ deck.name }}</router-link>
-                            <router-link :to="'/decks/build/' + deck.slug" class="block link-white p-4 pr-0" v-else>{{ deck.name }}</router-link>
+                            <router-link :to="'/decks/build/' + deck.slug" class="block link-white flex-1 p-4 pr-0" v-else>{{ deck.name }}</router-link>
                             <span v-if="user.subscription">
                                 <router-link :to="'/decks/build/' + deck.slug" class="block link-white p-4 pr-0">Classic</router-link>
                             </span>
