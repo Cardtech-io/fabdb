470255166a014303563cce76b602dec130488ebd
Kirk Bushell <kirk@awardforce.com>
2020-09-11 12:30:54 +1000

    New buttons, colour card management component and more.

---
+++ resources/js/DeckBuilder/Colour.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="inline-block rounded-lg h-2 w-2" :class="resourceColour(resource)" v-if="resource"></div>
+</template>
+
+<script>
+    import Cardable from "../CardDatabase/Cardable";
+
+    export default {
+        props: ['resource'],
+        mixins: [Cardable]
+    }
+</script>
