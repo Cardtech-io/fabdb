82e3bccc6fab3ae5dab59aa1b7912bab685708a3
Kirk Bushell <kirk@awardforce.com>
2020-12-02 13:41:59 +1100

    Added google adsense.

--- config/services.php
+++ config/services.php
@@ -13,6 +13,9 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
+    'adsense' => [
+        'client' => 'ca-pub-7143493109878588'
+    ],
 
     'aws' => [
         'cloudfront' => [
