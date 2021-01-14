bcee39fa8671f1bb7f353b6f205a21e9e30a56b3
Kirk Bushell <kirk@awardforce.com>
2021-01-15 01:04:52 +1100

    Rarity symbols, css updates, all for collections.

--- resources/js/CardDatabase/CardListItem.vue
+++ resources/js/CardDatabase/CardListItem.vue
@@ -7,7 +7,7 @@
             <td class="border border-gray-300 px-2 sm:pl-8 text-right"><span :class="printing.sku.finish" class="inline-block text-sm text-white px-1 mr-1 rounded-sm" :title="printing.sku.finish">{{ printing.sku.sku }}</span></td>
             <td class="border border-gray-300 px-2 hidden sm:table-cell">{{ englishFinish(printing.sku.finish) }}</td>
             <td class="border border-gray-300 px-2 hidden sm:table-cell">{{ printing.sku.set.name }}</td>
-            <td class="border border-gray-300 px-2 hidden sm:table-cell">{{ card.rarity }}</td>
+            <td class="border border-gray-300 px-2 hidden text-center sm:table-cell"><rarity-symbol :rarity="card.rarity"></rarity-symbol></td>
             <td class="border border-gray-300 text-center"><card-count :printing="printing"></card-count></td>
             <td class="border border-gray-300 text-center py-2 align-middle">
                 <input type="checkbox" class="form-checkbox h-5 w-5 text-green-500 focus:outline-none" v-model="printing.trade" @click="toggle(printing, 'trade')">
@@ -24,11 +24,13 @@
     import Cardable from './Cardable.js';
     import CardCount from '../Collection/CardCount.vue';
     import Icon from "../Components/Icon";
+    import RaritySymbol from "./RaritySymbol";
 
     export default {
         components: {
             CardCount,
-            Icon
+            Icon,
+            RaritySymbol,
         },
 
         mixins: [Cardable],
