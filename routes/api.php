47b399f5d46123fd7d97e0e94b6ed153c475f50a
Kirk Bushell <kirk@awardforce.com>
2021-01-19 08:53:27 +1100

    API now supports card slug search.

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::group(['middleware' => LogRequests::class], function() {
     Route::get('cards', 'CardController@list');
     Route::get('cards/first', 'CardController@first');
     Route::get('cards/pack', 'CardController@generatePack');
-    Route::get('cards/{card}', 'CardController@view');
+    Route::get('cards/{cardIdentifier}', 'CardController@view');
 
     Route::get('decks/{deck}', 'DeckController@view');
     Route::get('decks/{deck}/osc', 'DeckController@osc');
