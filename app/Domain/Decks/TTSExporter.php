ade21bd660fdbf616d7035794ea2528961816f32
Kirk Bushell <kirk@awardforce.com>
2021-06-09 17:05:07 +1000

    Fixed TTS exporter.

--- app/Domain/Decks/TTSExporter.php
+++ app/Domain/Decks/TTSExporter.php
@@ -158,7 +158,7 @@ class TTSExporter
         $this->deck->load('cards.printings');
 
         $images = $this->deck->cards->map(function(Card $card) {
-            return $this->cardImagePath($card->image);
+            return $this->cardImageFromSku($card->image);
         })->toArray();
 
         // Now we push the "hidden" card back:
@@ -167,13 +167,18 @@ class TTSExporter
         return $images;
     }
 
-    private function cardImagePath(Sku $sku): string
+    private function cardImageFromSku(Sku $sku): string
     {
         list($set, $id) = str_split(strtolower($sku->stripped()), 3);
 
         $id = preg_replace('/^0{1,2}/', '', $id);
 
         return Storage::disk('do')->path("$set/$id.png");
+    }
+
+    private function cardImageFromCard(Card $card)
+    {
+        return Storage::disk('do')->path("{$card->image}.png");
     }
 
     /**
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -44,11 +44,11 @@ $jsFile = $view === 'embed' ? 'embed' : 'app';
             </script>
         @endif
 
-        @if (config('services.hotjar.id'))
+        @if ($hotjarId = config('services.hotjar.id'))
             <script>
                 (function(h,o,t,j,a,r){
                     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:2444301,hjsv:6};
+                    h._hjSettings={hjid:{{$hotjarId}},hjsv:6};
                     a=o.getElementsByTagName('head')[0];
                     r=o.createElement('script');r.async=1;
                     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
