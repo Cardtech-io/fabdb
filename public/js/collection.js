383c6b7646df942050d604985af67038821f6089
Kirk Bushell <kirk@awardforce.com>
2021-08-25 11:19:51 +1000

    Small update for viewing collections on mobile.:

--- public/js/collection.js
+++ public/js/collection.js
@@ -277,6 +277,12 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 //
 //
 //
+//
+//
+//
+//
+//
+//
 
 
 
@@ -905,7 +911,31 @@ var render = function() {
                       }
                     }
                   },
-                  [_vm._v("My want/trade list")]
+                  [
+                    _c("span", { staticClass: "hidden sm:inline" }, [
+                      _vm._v("My want/trade list")
+                    ]),
+                    _vm._v(" "),
+                    _c(
+                      "icon",
+                      { staticClass: "sm:hidden -mt-1", attrs: { size: 8 } },
+                      [
+                        _c("path", {
+                          attrs: { d: "M10 12a2 2 0 100-4 2 2 0 000 4z" }
+                        }),
+                        _vm._v(" "),
+                        _c("path", {
+                          attrs: {
+                            "fill-rule": "evenodd",
+                            d:
+                              "M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z",
+                            "clip-rule": "evenodd"
+                          }
+                        })
+                      ]
+                    )
+                  ],
+                  1
                 )
               ],
               1
