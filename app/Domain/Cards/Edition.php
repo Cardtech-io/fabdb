9756d776e7676540ebfe72dcf343e12dd8426f3f
Kirk Bushell <kirk@awardforce.com>
2022-04-18 09:37:17 +1000

    Updated files for the card database

--- app/Domain/Cards/Edition.php
+++ app/Domain/Cards/Edition.php
@@ -13,7 +13,7 @@ class Edition
 
     public function __construct(?string $edition)
     {
-        Assert::inArray($edition, [null, 'Alpha Print', '1st Edition', 'Unlimited', 'Slingshot Underground']);
+        Assert::inArray($edition, [null, '', 'Alpha Print', '1st Edition', 'Unlimited', 'Slingshot Underground']);
 
         $this->edition = $edition;
     }
