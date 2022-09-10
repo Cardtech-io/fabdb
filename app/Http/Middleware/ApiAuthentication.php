8bddd5c0d8c48262cc36e29339b74996c5875c9f
Kirk Bushell <kirk@awardforce.com>
2022-09-10 13:23:37 +1000

    Added some logging to game results.

--- app/Http/Middleware/ApiAuthentication.php
+++ app/Http/Middleware/ApiAuthentication.php
@@ -16,7 +16,10 @@ class ApiAuthentication
     {
         $token = str_replace('Bearer ', '', $request->header('Authorization'));
         $user = $this->users->findByApiToken($token);
-
+        info('Game result request user: ['.$user->id.']');
+        info('Game result request time: ['.$request->get('time').']');
+        info('Game result request hash: ['.$request->get('hash').']');
+        info('Game result request token: ['.$request->header('Authorization').']');
         $hash = hash('sha512', $user->apiSecret.$request->get('time'));
 
         if ($hash === $request->get('hash')) {
