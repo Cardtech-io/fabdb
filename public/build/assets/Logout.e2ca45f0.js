7ef2ebc5ae8c7af71bcdd1dc780d9f64073993ea
Kirk Bushell <kirk@awardforce.com>
2022-11-09 13:32:37 +1100

    Import tweaks for dynasty.

---
+++ public/build/assets/Logout.e2ca45f0.js
@@ -0,0 +1,1 @@
+import{a as n,p as t,n as o}from"./app.f6f0f371.js";const r={mounted(){n.delete("/auth/session").then(()=>{window.session.user=null,this.setUser({user:null}),this.$router.push("/")})},methods:{...t("session",["setUser"])}};var a=function(){var s=this,e=s._self._c;return e("div")},l=[],u=o(r,a,l,!1,null,null,null,null);const c=u.exports;export{c as default};
