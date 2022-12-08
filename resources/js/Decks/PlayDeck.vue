ccd27e08eb757e3011ad9bdec975c4e2782bfe87
Kirk Bushell <kirk@awardforce.com>
2022-12-08 20:32:39 +1100

    Updated to play button to add felt table support.

--- resources/js/Decks/PlayDeck.vue
+++ resources/js/Decks/PlayDeck.vue
@@ -18,6 +18,14 @@
                 </a>
             </selector-item>
             <selector-item value="test" :selected="slotProps.selected">
+                <a :href="feltTableUrl()" target="_blank" class="flex items-center space-x-1">
+                    <icon :size="4">
+                        <path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z" />
+                    </icon>
+                    <span>Play on Felt Table</span>
+                </a>
+            </selector-item>
+            <selector-item value="test" :selected="slotProps.selected">
                 <router-link :to="{name: 'decks.test', params: {deck: deck.slug}}" class="flex items-center space-x-1 lg:hidden">
                     <icon :size="4">
                         <path d="M12.75 4a.75.75 0 00-.75.75v10.5c0 .414.336.75.75.75h.5a.75.75 0 00.75-.75V4.75a.75.75 0 00-.75-.75h-.5zM17.75 4a.75.75 0 00-.75.75v10.5c0 .414.336.75.75.75h.5a.75.75 0 00.75-.75V4.75a.75.75 0 00-.75-.75h-.5zM3.288 4.819A1.5 1.5 0 001 6.095v7.81a1.5 1.5 0 002.288 1.277l6.323-3.906a1.5 1.5 0 000-2.552L3.288 4.819z" />
@@ -65,6 +73,10 @@
                 let visibility = this.deck.visiblity ? this.deck.visibility : 'public';
 
                 return `https://www.fleshandbloodonline.com/FaBOnline2/CreateGame.php?fabdb=${fabDeckUrl}&user=${this.user.slug}&gameDescription=${description}&format=${this.deck.format}&visibility=${visibility}`;
+            },
+
+            feltTableUrl() {
+                return `https://felttable.com/fab/import?svc=fabdb&id=${this.deck.slug}`;
             }
         }
     }
