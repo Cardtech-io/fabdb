3f6b9e6d133b3b69d23138d1e2f302e177c47e70
Michael Matias <michael.f.matias@gmail.com>
2023-01-09 19:41:08 +0000

    Fixed talishar issue and other two issues.
    Fixed the following issues:
    Responsiveness of new deck in mobile. Now displays in single row with breadcrumbs.
    Color of the play menu was not visible since text color was same as background. Added corresponding theme color to all themes.
    Added random string to match description on Talishar.

--- resources/js/Components/Breadcrumbs.vue
+++ resources/js/Components/Breadcrumbs.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="crumbs font-serif uppercase">
-        <div class="container sm:mx-auto p-4 py-2 sm:flex items-center justify-between">
+        <div class="container sm:mx-auto p-4 py-2 sm:flex items-center justify-between flex flex-row">
             <crumbs :crumbs="crumbs"/>
             <slot></slot>
         </div>
--- resources/js/Decks/PlayDeck.vue
+++ resources/js/Decks/PlayDeck.vue
@@ -1,5 +1,5 @@
 <template>
-    <selector :width="170" layout="icon">
+    <selector :width="170" layout="icon" class="playdeck">
         <template #title>
             <div class="flex space-x-1 link">
                 <icon :size="6">
@@ -9,7 +9,7 @@
             </div>
         </template>
         <template v-slot:items="slotProps">
-            <selector-item value="test" :selected="slotProps.selected">
+            <selector-item value="test" :selected="slotProps.selected" class="dark">
                 <a :href="fabOnlineUrl()" target="_blank" class="flex items-center space-x-1">
                     <icon :size="4">
                         <path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z" />
@@ -17,7 +17,7 @@
                     <span>Play on Talishar</span>
                 </a>
             </selector-item>
-            <selector-item value="test" :selected="slotProps.selected">
+            <selector-item value="test" :selected="slotProps.selected" class="dark">
                 <a :href="feltTableUrl()" target="_blank" class="flex items-center space-x-1">
                     <icon :size="4">
                         <path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z" />
@@ -25,14 +25,14 @@
                     <span>Play on Felt Table</span>
                 </a>
             </selector-item>
-            <selector-item value="test" :selected="slotProps.selected">
+            <selector-item value="test" :selected="slotProps.selected" class="dark">
                 <router-link :to="{name: 'decks.test', params: {deck: deck.slug}}" class="flex items-center space-x-1 lg:hidden">
                     <icon :size="4">
                         <path d="M12.75 4a.75.75 0 00-.75.75v10.5c0 .414.336.75.75.75h.5a.75.75 0 00.75-.75V4.75a.75.75 0 00-.75-.75h-.5zM17.75 4a.75.75 0 00-.75.75v10.5c0 .414.336.75.75.75h.5a.75.75 0 00.75-.75V4.75a.75.75 0 00-.75-.75h-.5zM3.288 4.819A1.5 1.5 0 001 6.095v7.81a1.5 1.5 0 002.288 1.277l6.323-3.906a1.5 1.5 0 000-2.552L3.288 4.819z" />
                     </icon>
                     <span>Deck tester</span>
                 </router-link>
-                <router-link :to="{name: 'decks.test.prepare', params: {deck: deck.slug}}" class="items-center space-x-1 hidden lg:flex hover:text-gray-400 dark:hover:text-white">
+                <router-link :to="{name: 'decks.test.prepare', params: {deck: deck.slug}}" class="items-center space-x-1 hidden lg:flex">
                     <icon :size="4">
                         <path d="M12.75 4a.75.75 0 00-.75.75v10.5c0 .414.336.75.75.75h.5a.75.75 0 00.75-.75V4.75a.75.75 0 00-.75-.75h-.5zM17.75 4a.75.75 0 00-.75.75v10.5c0 .414.336.75.75.75h.5a.75.75 0 00.75-.75V4.75a.75.75 0 00-.75-.75h-.5zM3.288 4.819A1.5 1.5 0 001 6.095v7.81a1.5 1.5 0 002.288 1.277l6.323-3.906a1.5 1.5 0 000-2.552L3.288 4.819z" />
                     </icon>
@@ -69,7 +69,8 @@
         methods: {
             fabOnlineUrl() {
                 let fabDeckUrl = encodeURIComponent('https://fabdb.net/decks/build/'+this.deck.slug);
-                let description = encodeURIComponent('Testing '+this.deck.name);
+                let randomSlug = Math.random().toString(36).slice(2);
+                let description = encodeURIComponent('FabDB-'+randomSlug);
                 let visibility = this.deck.visiblity ? this.deck.visibility : 'public';
 
                 return `https://www.fleshandbloodonline.com/game/CreateGame.php?fabdb=${fabDeckUrl}&user=${this.user.slug}&gameDescription=${description}&format=${this.deck.format}&visibility=${visibility}`;
